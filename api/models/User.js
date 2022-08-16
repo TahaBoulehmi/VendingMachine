4092edca3b61bfafb2c6a13df193b9dac46cb623
TahaBoulehmi <contact@btaha.com>
2022-08-16 12:34:46 +0200

    User Model

---
+++ api/models/User.js
@@ -0,0 +1,77 @@
+/**
+ * User.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+const Passwords = require('machinepack-passwords')
+
+module.exports = {
+  attributes: {
+    username: {
+      type: 'string',
+      required: true,
+      unique: true,
+      maxLength: 25,
+      isNotEmptyString: true,
+    },
+    password: {
+      type: 'string',
+      required: true,
+      isNotEmptyString: true,
+    },
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+  },
+  beforeCreate: function (values, proceed) {
+    values.username ? (values.username = sails.helpers.format.with({ usernam: values.username })) : null
+    if (values.password) {
+      Passwords.encryptPassword({
+        password: values.password,
+      }).exec({
+        error: function () {
+          values.password = undefined
+          return proceed()
+        },
+        success: function (encPass) {
+          values.password = encPass
+          return proceed()
+        },
+      })
+    } else {
+      return proceed()
+    }
+  },
+
+  beforeUpdate: function (values, proceed) {
+    values.username ? (values.username = sails.helpers.format.with({ usernam: values.username })) : null
+    if (values.password) {
+      Passwords.encryptPassword({
+        password: values.password,
+      }).exec({
+        error: function () {
+          values.password = undefined
+          return proceed()
+        },
+
+        success: function (encPass) {
+          values.password = encPass
+          return proceed()
+        },
+      })
+    } else {
+      return proceed()
+    }
+  },
+}
