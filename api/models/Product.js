fd1da052316462d173162ba4938e9893a43a636f
TahaBoulehmi <contact@btaha.com>
2022-08-16 13:21:50 +0200

    Product API

---
+++ api/models/Product.js
@@ -0,0 +1,41 @@
+/**
+ * Product.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    name: {
+      type: 'string',
+      required: true,
+      isNotEmptyString: true,
+    },
+    cost: {
+      type: 'number',
+      required: true,
+      min: 0,
+    },
+    amountAvailable: {
+      type: 'number',
+      required: true,
+      isInteger: true,
+      min: 0,
+    },
+    seller: {
+      model: 'user',
+    },
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+  },
+}
--- api/models/User.js
+++ api/models/User.js
@@ -21,6 +21,10 @@ module.exports = {
       required: true,
       isNotEmptyString: true,
     },
+    products: {
+      collection: 'product',
+      via: 'seller',
+    },
 
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
