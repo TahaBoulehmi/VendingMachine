d751eb3e4933399afcfbf09570c5902a703b2be6
TahaBoulehmi <contact@btaha.com>
2022-08-18 13:26:30 +0200

    Queries

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import Signin from './containers/Signin'
 import Signup from './containers/Signup'
 import NotFound from './containers/NotFound'
 import Dashboard from './containers/Dashboard'
+
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" exact element={<Dashboard />} />
-        <Route path="/signin" exact element={<Signin />} />
-        <Route path="/signup" exact element={<Signup />} />
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient} contextSharing={true}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Dashboard />} />
+          <Route path="/signin" exact element={<Signin />} />
+          <Route path="/signup" exact element={<Signup />} />
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 export default App
