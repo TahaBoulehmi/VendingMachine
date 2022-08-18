babadb317c177f609f446bf3e05a236ab61fdb36
TahaBoulehmi <contact@btaha.com>
2022-08-18 11:16:12 +0200

    Signup states

--- client/src/containers/Signup.js
+++ client/src/containers/Signup.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 
@@ -8,6 +8,13 @@ function classNames(...classes) {
 }
 
 export default function Signup() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [role, setRole] = useState({ name: 'Buyer', id: 0 })
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+
   return (
     <>
       <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -30,9 +37,9 @@ export default function Signup() {
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6" action="#" method="POST">
+            <form className="space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
               <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                <label htmlFor="name" className="block text-sm font-medium text-gray-700">
                   Username
                 </label>
                 <div className="mt-1">
@@ -41,6 +48,8 @@ export default function Signup() {
                     name="username"
                     type="text"
                     autoComplete="username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   />
@@ -56,7 +65,9 @@ export default function Signup() {
                     id="password"
                     name="password"
                     type="password"
-                    autoComplete="current-password"
+                    autoComplete="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   />
@@ -66,7 +77,7 @@ export default function Signup() {
               <Menu as="div" className="relative inline-block text-left">
                 <div>
                   <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                    Role
+                    {role.name}
                     <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
                   </Menu.Button>
                 </div>
@@ -83,30 +94,29 @@ export default function Signup() {
                   <Menu.Items className="origin-top-right absolute mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <div className="py-1">
                       <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                              'block px-4 py-2 text-sm'
-                            )}
-                          >
-                            Buyer
-                          </a>
-                        )}
+                        <a
+                          href={() => false}
+                          onClick={() => setRole({ name: 'Buyer', id: 0 })}
+                          className={classNames(
+                            role.id === 0 ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                            'block px-4 py-2 text-sm'
+                          )}
+                        >
+                          {' '}
+                          Buyer
+                        </a>
                       </Menu.Item>
                       <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                              'block px-4 py-2 text-sm'
-                            )}
-                          >
-                            Seller
-                          </a>
-                        )}
+                        <a
+                          href={() => false}
+                          onClick={() => setRole({ name: 'Seller', id: 1 })}
+                          className={classNames(
+                            role.id === 1 ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                            'block px-4 py-2 text-sm'
+                          )}
+                        >
+                          Seller
+                        </a>
                       </Menu.Item>
                     </div>
                   </Menu.Items>
@@ -116,6 +126,7 @@ export default function Signup() {
               <div>
                 <button
                   type="submit"
+                  onClick={handleSubmit}
                   className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   Sign up
