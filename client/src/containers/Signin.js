906d07333bb5e0ab90e0bfca94409948dc54ab15
TahaBoulehmi <contact@btaha.com>
2022-08-18 11:17:35 +0200

    Signin states

--- client/src/containers/Signin.js
+++ client/src/containers/Signin.js
@@ -1,6 +1,12 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export default function Signin() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
   return (
     <>
       <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -23,9 +29,9 @@ export default function Signin() {
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6" action="#" method="POST">
+            <form className="space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
               <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                <label htmlFor="username" className="block text-sm font-medium text-gray-700">
                   Username
                 </label>
                 <div className="mt-1">
@@ -34,12 +40,13 @@ export default function Signin() {
                     name="username"
                     type="text"
                     autoComplete="username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   />
                 </div>
               </div>
-
               <div>
                 <label htmlFor="password" className="block text-sm font-medium text-gray-700">
                   Password
@@ -50,6 +57,8 @@ export default function Signin() {
                     name="password"
                     type="password"
                     autoComplete="current-password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   />
@@ -58,6 +67,7 @@ export default function Signin() {
               <div>
                 <button
                   type="submit"
+                  onClick={handleSubmit}
                   className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                   Sign in
